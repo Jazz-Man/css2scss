c29bb7c5e9b71fcc1258c41a442ded291e82ce49
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-02-26 21:37:53 +0200

    Add JSDoc to parseCSS

--- src/core/parser.js
+++ src/core/parser.js
@@ -1,5 +1,11 @@
 import postcss from "postcss";
 
+/**
+ * Parses a CSS string into a PostCSS root node.
+ *
+ * @param {string} cssString - The CSS string to parse.
+ * @returns {import('postcss').Root} The PostCSS root node representing the parsed CSS.
+ */
 export function parseCSS(cssString) {
 	try {
 		const root = postcss.parse(cssString, {
