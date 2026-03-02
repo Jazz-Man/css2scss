3c791f7ed8984eb2f24bf9b7cf806c528cef02c3
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-03-02 18:17:16 +0200

    Extract reduce-based transformer to poc module

--- test.js
+++ test.js
@@ -1,81 +1,11 @@
 import postcss from "postcss";
 import selectorParser from "postcss-selector-parser";
 
+import { transformSelectorReduce } from "./src/poc/reduce-transformer.js";
+
 import debug from "./src/utils/debug";
 
-const newRoot = postcss.root();
+const result = transformSelectorReduce(".test, .item:hover, .link.active");
 
-const loremDecl = postcss.decl({ prop: "width", value: "378px" });
-
-/**
- * @param {import('postcss-selector-parser').Node} node
- * @returns {string}
- */
-function getNodeSelector(node) {
-	const prevNode = node.prev();
-
-	const nodeSelector = node.toString();
-
-	if (!prevNode) {
-		return nodeSelector;
-	}
-
-	return prevNode?.type === "combinator" && prevNode?.value === " "
-		? nodeSelector
-		: `&${nodeSelector}`;
-}
-
-selectorParser((selectors) => {
-	const sel = selectors.at(0);
-	if (!sel) return;
-
-	if (sel.length > 1) {
-		const lastIndex = sel.last.sourceIndex;
-
-		sel.reduce((prevRule, node) => {
-			const prevNode = node.prev();
-
-			if (node.sourceIndex === lastIndex) {
-				if (prevRule) {
-					const newRule = postcss
-						.rule({ selector: getNodeSelector(node) })
-						.append(loremDecl);
-
-					prevRule.append(newRule);
-
-					return newRule;
-				}
-			}
-
-			if (node.type === "combinator" && node.value === " ") {
-				return prevRule;
-			}
-
-			if (!prevNode) {
-				/** @type {import('postcss').Rule} */
-				prevRule = postcss.rule({ selector: getNodeSelector(node) });
-				newRoot.append(prevRule);
-
-				return prevRule;
-			}
-
-			const newRule = postcss.rule({ selector: getNodeSelector(node) });
-
-			prevRule.append(newRule);
-
-			return newRule;
-		}, undefined);
-	} else {
-		newRoot.append(
-			postcss.rule({ selector: getNodeSelector(sel.first) }).append(loremDecl),
-		);
-	}
-}).processSync(
-	// ".ArticleCard_card",
-	// ".ArticleCard_card .Test",
-	// ".ArticleCard_card, .Test",
-	".ArticleCard_card.Test",
-	// '.ArticleCard_card.Test [data-test="some-value"] .Some-more-selectors #id-element div .more-selectors.and-more:hover .test-last-class:before',
-);
-
-console.log(newRoot.toString());
+console.log(result.toString());
+// console.log(newRoot.toString());
